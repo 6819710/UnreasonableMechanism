99d3cd182c6453de5f60cc6569c10eac9fe57b06
6819710 <j.p.galovic@gmail.com>
2015-11-02 15:06:57 +1100

    Add: Setup inital engine library

---
+++ UnresonableMechanismEngineCSv0.2/src/BasicMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnreasonableMechanismEngineCS
+{
+    /// <summary>
+    /// BasicMath provides common math formules.
+    /// </summary>
+    public static class BasicMath
+    {
+        public static double ToRad(double angle)
+        {
+            return angle * (Math.PI / 180);
+        }
+    }
+}
