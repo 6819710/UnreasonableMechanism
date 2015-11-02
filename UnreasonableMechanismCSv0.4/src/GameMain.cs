ab08c024bdb31bfcada4c85d46b69c6948eb30da
6819710 <j.p.galovic@gmail.com>
2015-11-02 15:34:34 +1100

    Add: Added main game vars.

--- UnreasonableMechanismCSv0.4/src/GameMain.cs
+++ UnreasonableMechanismCSv0.4/src/GameMain.cs
@@ -3,23 +3,40 @@ using SwinGameSDK;
 
 namespace UnreasonableMechanismCS
 {
+    /// <summary>
+    /// GameMain defines the main access point for the program.
+    /// </summary>
     public class GameMain
     {
+        private static string _title = "Unresonable Mechanism";
+        private static double _version = 0.4;
+        private static int _tick = 0;
+
+        private static bool _splashFlag = false;
+
+        /// <summary>
+        /// Main access point for the program.
+        /// </summary>
         public static void Main()
         {
-            //Open the game window
-            SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
-            SwinGame.ShowSwinGameSplashScreen();
+            //Open game window.
+            SwinGame.OpenGraphicsWindow(_title + " v" + _version, 800, 600);
 
-            //Run the game loop
+            //Show SwinGame splash screen.
+            if(_splashFlag)
+            {
+                SwinGame.ShowSwinGameSplashScreen();
+            }
+
+            //Run game loop.
             while (false == SwinGame.WindowCloseRequested())
             {
                 //Fetch the next batch of UI interaction
                 SwinGame.ProcessEvents();
 
                 //Clear the screen and draw the framerate
-                SwinGame.ClearScreen(Color.White);
-                SwinGame.DrawFramerate(0, 0);
+                SwinGame.ClearScreen(Color.Black);
+                SwinGame.DrawFramerate(2, 2);
 
                 //Draw onto the screen
                 SwinGame.RefreshScreen(60);
