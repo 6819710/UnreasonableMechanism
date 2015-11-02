eac0aabc9476e5f1072f4124d4317ea7432f8609
6819710 <j.p.galovic@gmail.com>
2015-11-02 15:18:58 +1100

    Add: Added Angle Convertions to BasicMath.

--- UnresonableMechanismEngineCSv0.2/src/BasicMath.cs
+++ UnresonableMechanismEngineCSv0.2/src/BasicMath.cs
@@ -11,9 +11,24 @@ namespace UnreasonableMechanismEngineCS
     /// </summary>
     public static class BasicMath
     {
+        /// <summary>
+        /// Converts angles from degrees to radians.
+        /// </summary>
+        /// <param name="angle">Angle in degrees.</param>
+        /// <returns>Angle in radians</returns>
         public static double ToRad(double angle)
         {
             return angle * (Math.PI / 180);
+        }
+
+        /// <summary>
+        /// Converts angles fron radians to degrees.
+        /// </summary>
+        /// <param name="angle">Angle in radians.</param>
+        /// <returns>Angle in degrees.</returns>
+        public static double ToDeg(double angle)
+        {
+            return angle * (180 / Math.PI);
         }
     }
 }
