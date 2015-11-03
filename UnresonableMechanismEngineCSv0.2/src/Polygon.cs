c640b9170471b9bb2d2c2c1681b3763be896171e
6819710 <j.p.galovic@gmail.com>
2015-11-04 05:55:06 +1100

    Add: Feilds, Defult contstructor and accessors, Added Center Property

--- UnresonableMechanismEngineCSv0.2/src/Polygon.cs
+++ UnresonableMechanismEngineCSv0.2/src/Polygon.cs
@@ -10,5 +10,64 @@ namespace UnreasonableMechanismEngineCS
     /// </summary>
     public class Polygon
     {
+        private List<Point> _vertices;
+        private List<Vector> _edges;
+
+        /// <summary>
+        /// Constructs an empty polygon.
+        /// </summary>
+        public Polygon()
+        {
+            _vertices = new List<Point>();
+            _edges = new List<Vector>();
+        }
+
+        /// <summary>
+        /// Constructs a polygon using an array of vertices.
+        /// </summary>
+        /// <param name="vertices">Array of vertices (point).</param>
+        public Polygon(Point[] vertices)
+        {
+            _vertices = new List<Point>(vertices);
+            _edges = new List<Vector>();
+        }
+
+        /// <summary>
+        /// Readonly Property: Center point of polygon (calculated).
+        /// </summary>
+        public Point center
+        {
+            get
+            {
+                Point result = new Point();
+                foreach(Point vertex in _vertices)
+                {
+                    result += vertex;
+                }
+                return result / _vertices.Count;
+            }
+        }
+
+        /// <summary>
+        /// Readonly Property: List of edges (vector).
+        /// </summary>
+        public List<Vector> Edges
+        {
+            get
+            {
+                return _edges;
+            }
+        }
+
+        /// <summary>
+        /// Readonly Property: List of vertices (point).
+        /// </summary>
+        public List<Point> Vertices
+        {
+            get
+            {
+                return _vertices;
+            }
+        }
     }
 }
--- UnresonableMechanismEngineCSv0.2/src/Polyhedron.cs
+++ UnresonableMechanismEngineCSv0.2/src/Polyhedron.cs
@@ -10,5 +10,6 @@ namespace UnreasonableMechanismEngineCS
     /// </summary>
     public class Polyhedron
     {
+
     }
 }
