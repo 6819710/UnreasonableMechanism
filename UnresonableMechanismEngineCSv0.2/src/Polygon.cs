81532b288ffb316ad4b563f3c5604f30368a6dfe
6819710 <j.p.galovic@gmail.com>
2015-11-04 05:31:17 +1100

    Add: base classes for Polygon and Polyhedron.

---
+++ UnresonableMechanismEngineCSv0.2/src/Polygon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismEngineCS
+{
+    /// <summary>
+    /// Polygon defines 2D objects made up of an equal number of vertices (points) and edges (vectors).
+    /// </summary>
+    public class Polygon
+    {
+    }
+}
---
+++ UnresonableMechanismEngineCSv0.2/src/Polyhedron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismEngineCS
+{
+    /// <summary>
+    /// Polyhedron defines 3D objects made up of 2D faces (polygon).
+    /// </summary>
+    public class Polyhedron
+    {
+    }
+}
