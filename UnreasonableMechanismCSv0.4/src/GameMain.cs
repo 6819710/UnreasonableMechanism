1fcaf431bed105eb6ba3cbcfe18fa1fc09282eb5
6819710 <j.p.galovic@gmail.com>
2015-11-05 17:11:46 +1100

    Add: Visual test for polyhedron.

--- UnreasonableMechanismCSv0.4/src/GameMain.cs
+++ UnreasonableMechanismCSv0.4/src/GameMain.cs
@@ -19,14 +19,17 @@ namespace UnreasonableMechanismCS
         /// </summary>
         public static void Main()
         {
-            Polygon polygon = new Polygon(new Point[]
-            {
-                new Point(0, 0),
-                new Point(20, 0),
-                new Point(0, 80)
-            }, new Point(100, 100));
+            Point A = new Point(0, 0, 0);
+            Point B = new Point(1, 0, 0);
+            Point C = new Point(0.5, 0.866, 0);
+            Point D = new Point(0.5, 0.433, 0.866);
 
+            Polygon A1 = new Polygon(new Point[] { A * 50, B * 50, C * 50 });
+            Polygon A2 = new Polygon(new Point[] { A * 50, B * 50, D * 50 });
+            Polygon A3 = new Polygon(new Point[] { B * 50, C * 50, D * 50 });
+            Polygon A4 = new Polygon(new Point[] { C * 50, A * 50, D * 50 });
 
+            Polyhedron P1 = new Polyhedron(new Polygon[] { A1, A2, A3, A4 });
 
             //Open game window.
             SwinGame.OpenGraphicsWindow(_title + " v" + _version, 800, 600);
@@ -43,16 +46,16 @@ namespace UnreasonableMechanismCS
                 //Fetch the next batch of UI interaction
                 SwinGame.ProcessEvents();
 
-                polygon.Offset(new UnreasonableMechanismEngineCS.Vector(1, 1));
+                P1.Offset(new UnreasonableMechanismEngineCS.Vector(1, 1));
 
                 //Clear the screen and draw the framerate
                 SwinGame.ClearScreen(Color.Black);
                 SwinGame.DrawFramerate(2, 2);
-                
-                polygon.DrawFace(Color.Goldenrod);
-                polygon.DrawEdge(Color.DarkGoldenrod);
 
-                polygon.Center.Draw(Color.Red);
+                P1.DrawFace(Color.Goldenrod);
+                P1.DrawEdge(Color.DarkGoldenrod);
+
+                P1.Center.Draw(Color.Red);
 
                 //Draw onto the screen
                 SwinGame.RefreshScreen(60);
