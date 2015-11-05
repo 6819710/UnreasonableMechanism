97fd5e30fe9722106e6039867cf5afde878d9ca0
6819710 <j.p.galovic@gmail.com>
2015-11-05 15:46:03 +1100

    Remove: Auto generated vector and point tests.

--- UnresonableMechanismEngineCSv0.2/src/Polyhedron.cs
+++ UnresonableMechanismEngineCSv0.2/src/Polyhedron.cs
@@ -10,6 +10,66 @@ namespace UnreasonableMechanismEngineCS
     /// </summary>
     public class Polyhedron
     {
+        private List<Polygon> _faces;
 
+        /// <summary>
+        /// Constructs an empty polyhedron.
+        /// </summary>
+        public Polyhedron()
+        {
+            _faces = new List<Polygon>();
+        }
+
+        /// <summary>
+        /// Constructs a polyhedron with the provided faces.
+        /// </summary>
+        /// <param name="faces">Array of faces (polygon)</param>
+        public Polyhedron(Polygon[] faces)
+        {
+            _faces = new List<Polygon>(faces);
+        }
+
+        /// <summary>
+        /// Constructs a polyhedron with the provided faces at the given location
+        /// </summary>
+        /// <param name="faces">Array of faces (polygon)</param>
+        /// <param name="location">Location (point)</param>
+        public Polyhedron(Polygon[] faces, Point location)
+        {
+            _faces = new List<Polygon>(faces);
+            Offset(new Vector(Center, location));
+        }
+
+        /// <summary>
+        /// Readonly Property: Center point of polyhedron (calculated).
+        /// </summary>
+        public Point Center
+        {
+            get
+            {
+                Point result = new Point();
+                int count = 0;
+                foreach(Polygon face in _faces)
+                {
+                    foreach(Point vertex in face.Vertices)
+                    {
+                        result += vertex;
+                        count++;
+                    }
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Readonly Property: Faces (polygon).
+        /// </summary>
+        public List<Polygon> Faces
+        {
+            get
+            {
+                return _faces;
+            }
+        }
     }
 }
