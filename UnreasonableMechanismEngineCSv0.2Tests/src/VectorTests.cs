45046b0e28dcb136b2e49d5e728a82bc591d93ae
6819710 <j.p.galovic@gmail.com>
2015-11-07 02:03:09 +1100

    Add: Perpendicular Vector 2D test, Magnitude Test (vector)

---
+++ UnreasonableMechanismEngineCSv0.2Tests/src/VectorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using UnreasonableMechanismEngineCS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnreasonableMechanismEngineCS.Tests
+{
+    [TestFixture()]
+    public class VectorTests
+    {
+        [Test()]
+        public void MagnitudeTest()
+        {
+            Vector test = new Vector(3, 2);
+
+            Assert.AreEqual(Math.Sqrt(13), test.Magnitude, "Error: Magnitude of test vector should be the square route of 13.");
+        }
+
+        [Test()]
+        public void PerpendicularVector2DTest()
+        {
+            Vector test = new Vector(3, 2);
+            Vector perpendicular = test.PerpendicularVector2D();
+
+            Assert.AreEqual(0, test.DotProduct(perpendicular), "Error: the dot product of orthoginal vectors should be 0.");
+            Assert.AreEqual(test.Magnitude, perpendicular.Magnitude, "Error: the magnitude of orthoginal vectors should be the same.");
+        }
+    }
+}
