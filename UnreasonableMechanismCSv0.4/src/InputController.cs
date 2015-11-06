0f3d7921189d2704b9f7ca8b50656cdfa9e6b482
6819710 <j.p.galovic@gmail.com>
2015-11-07 04:37:19 +1100

    Ad: scale, yaw, pitch and roll

--- UnreasonableMechanismCSv0.4/src/InputController.cs
+++ UnreasonableMechanismCSv0.4/src/InputController.cs
@@ -17,6 +17,8 @@ namespace UnreasonableMechanismCS
         public static void ProcessInput()
         {
             HandleMovement();
+            HandleYawPitchRoll();
+            HandleScale();
         }
 
         private static void HandleMovement()
@@ -39,7 +41,8 @@ namespace UnreasonableMechanismCS
                 foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
                 {
                     polyhedron.Offset(new UM.Vector(0, 2));
-                    if(polyhedron.GreaterThanY(600))
+                    polyhedron.RollX(-0.035, polyhedron.Center);
+                    if (polyhedron.GreaterThanY(600))
                     {
                         polyhedron.Offset(new UM.Vector(0, -polyhedron.MaxDistanceGreaterThanY(600)));
                     }
@@ -51,7 +54,8 @@ namespace UnreasonableMechanismCS
                 foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
                 {
                     polyhedron.Offset(new UM.Vector(-2, 0));
-                    if(polyhedron.LessThanX(0))
+                    polyhedron.PitchY(-0.035, polyhedron.Center);
+                    if (polyhedron.LessThanX(0))
                     {
                         polyhedron.Offset(new UM.Vector(polyhedron.MaxDistanceLessThanX(0), 0));
                     }
@@ -63,9 +67,88 @@ namespace UnreasonableMechanismCS
                 foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
                 {
                     polyhedron.Offset(new UM.Vector(2, 0));
-                    if(polyhedron.GreaterThanX(800))
+                    polyhedron.PitchY(0.035, polyhedron.Center);
+                    if (polyhedron.GreaterThanX(800))
                     {
                         polyhedron.Offset(new UM.Vector(-polyhedron.MaxDistanceGreaterThanX(800), 0));
+                    }
+                }
+            }
+        }
+
+        private static void HandleYawPitchRoll()
+        {
+            if (SwinGame.KeyDown(KeyCode.vk_w))
+            {
+                foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.RollX(0.035, polyhedron.Center);
+                }
+            }
+
+            if (SwinGame.KeyDown(KeyCode.vk_s))
+            {
+                foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.RollX(-0.035, polyhedron.Center);
+                }
+            }
+
+            if (SwinGame.KeyDown(KeyCode.vk_a))
+            {
+                foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.PitchY(-0.035, polyhedron.Center);
+                }
+            }
+
+            if (SwinGame.KeyDown(KeyCode.vk_d))
+            {
+                foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.PitchY(0.035, polyhedron.Center);
+                }
+            }
+
+            if (SwinGame.KeyDown(KeyCode.vk_q))
+            {
+                foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.YawZ(0.035, polyhedron.Center);
+                }
+            }
+
+            if (SwinGame.KeyDown(KeyCode.vk_e))
+            {
+                foreach(Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.YawZ(-0.035, polyhedron.Center);
+                }
+            }
+        }
+
+        public static void HandleScale()
+        {
+            if (SwinGame.KeyDown(KeyCode.vk_z))
+            {
+                foreach (Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.Scale(1.01, polyhedron.Center);
+                    if(polyhedron.MaxDistanceLessThanX(polyhedron.Center.X) > 150)
+                    {
+                        polyhedron.Scale(0.99, polyhedron.Center);
+                    }
+                }
+            }
+
+            if (SwinGame.KeyDown(KeyCode.vk_x))
+            {
+                foreach (Polyhedron polyhedron in GameObjects.Polyhedra)
+                {
+                    polyhedron.Scale(0.99, polyhedron.Center);
+                    if (polyhedron.MaxDistanceLessThanX(polyhedron.Center.X) < 20)
+                    {
+                        polyhedron.Scale(1.01, polyhedron.Center);
                     }
                 }
             }
