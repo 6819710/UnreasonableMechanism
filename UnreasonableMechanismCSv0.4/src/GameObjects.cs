82928c3c399e81b2071f5a6c0136f2a5c2971611
6819710 <j.p.galovic@gmail.com>
2015-11-07 04:10:51 +1100

    Add: polyhedra game object list.

---
+++ UnreasonableMechanismCSv0.4/src/GameObjects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
+
+namespace UnreasonableMechanismCS
+{
+    public static class GameObjects
+    {
+        public static List<Polyhedron> Polyhedra = new List<Polyhedron>();
+
+        public static void Draw(Color clrFace, Color clrEdge)
+        {
+            foreach(Polyhedron polyhedron in Polyhedra)
+            {
+                polyhedron.Draw(clrFace, clrEdge);
+            }
+        }
+    }
+}
