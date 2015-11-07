72473d405af4a422e231e1244fcdcf0752fbcf40
6819710 <j.p.galovic@gmail.com>
2015-11-08 02:20:50 +1100

    Add: inital add.

---
+++ UnreasonableMechanismCSv0.4/src/Model/Enum/BulletColour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismCS
+{
+    /// <summary>
+    /// Avalible colours for bullets.
+    /// </summary>
+    public enum BulletColour
+    {
+        Black,
+        Red,
+        Purple,
+        Blue,
+        Turquoise,
+        Green,
+        Yellow,
+        White
+    }
+}
---
+++ UnreasonableMechanismCSv0.4/src/Model/Enum/BulletType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismCS
+{
+    /// <summary>
+    /// Avalible types of bullets.
+    /// </summary>
+    public enum BulletType
+    {
+        Beam,
+        BigStar,
+        Crystal,
+        Dart,
+        HugeSphere,
+        LargeSphere,
+        Palse,
+        Ring,
+        Seed,
+        SmallRing,
+        SmallSphere,
+        Shere,
+        Star
+    }
+}
