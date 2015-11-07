51bae5ecb6a860d61a7eadab3ecb30e8d27c18e2
6819710 <j.p.galovic@gmail.com>
2015-11-08 01:10:01 +1100

    Add: enum definitions.

--- UnreasonableMechanismCSv0.4/src/Model/Enum/ItemType.cs
+++ UnreasonableMechanismCSv0.4/src/Model/Enum/ItemType.cs
@@ -5,7 +5,17 @@ using System.Text;
 
 namespace UnreasonableMechanismCS
 {
+    /// <summary>
+    /// ItemType defines the avalible types of collectable items in the game.
+    /// </summary>
     public enum ItemType
     {
+        BigPower,
+        Bomb,
+        FullPower,
+        Life,
+        Point,
+        Power,
+        Star
     }
 }
