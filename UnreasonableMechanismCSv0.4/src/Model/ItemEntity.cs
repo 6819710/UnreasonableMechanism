9d4cf9f3621855d20cdea0f5d88044e04c1aecfc
6819710 <j.p.galovic@gmail.com>
2015-11-08 01:56:13 +1100

    Add: InitBounding method.

--- UnreasonableMechanismCSv0.4/src/Model/ItemEntity.cs
+++ UnreasonableMechanismCSv0.4/src/Model/ItemEntity.cs
@@ -15,9 +15,59 @@ namespace UnreasonableMechanismCS
     {
         private ItemType _itemType;
 
+        /// <summary>
+        /// Constructs a new item entitiy.
+        /// </summary>
+        /// <param name="position">Position of the item (point).</param>
+        /// <param name="itemType">Type of item (itemtype).</param>
         public ItemEntity(Point position, ItemType itemType) : base (position, InitBounding(position, itemType), 1, "Item" + itemType.ToString() )
         {
             _itemType = itemType;
+        }
+
+        /// <summary>
+        /// Readonly Property: ItemType.
+        /// </summary>
+        public ItemType ItemType
+        {
+            get
+            {
+                return _itemType;
+            }
+        }
+
+        public override void DrawEntity()
+        {
+            base.DrawEntity();
+        }
+
+        public override void ProcessEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProcessMovement()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Polygon InitBounding(Point position, ItemType itemType)
+        {
+            double x = position.X;
+            double y = position.Y;
+
+            double width = GameResources.GameImage("Item" + itemType.ToString()).Width;
+            double Height = GameResources.GameImage("Item" + itemType.ToString()).Height;
+
+            Point[] vertices = new Point[]
+            {
+                new Point(x + width, y - Height),
+                new Point(x - width, y - Height),
+                new Point(x - width, y + Height),
+                new Point(x + width, y + Height),
+            };
+
+            return new Polygon(vertices);
         }
     }
 }
