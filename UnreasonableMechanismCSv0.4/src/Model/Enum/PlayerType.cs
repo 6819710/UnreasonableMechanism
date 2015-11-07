9c83af600d82bd5112c43b1cea9dfac2adaf0706
6819710 <j.p.galovic@gmail.com>
2015-11-08 02:32:54 +1100

    Add: player type enum

---
+++ UnreasonableMechanismCSv0.4/src/Model/Enum/PlayerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismCS
+{
+    /// <summary>
+    /// Defines player type.
+    /// </summary>
+    public enum PlayerType
+    {
+    }
+}
