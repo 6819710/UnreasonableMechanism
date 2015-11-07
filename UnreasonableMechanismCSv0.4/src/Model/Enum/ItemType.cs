9eaed6b9e58052ee0e06f2d50e840cb1bbfb9580
6819710 <j.p.galovic@gmail.com>
2015-11-08 01:08:21 +1100

    Add: ItemEntity class and Item Type enum.

--- UnreasonableMechanismCSv0.4/src/Model/Enum/ItemType.cs
+++ UnreasonableMechanismCSv0.4/src/Model/Enum/ItemType.cs
@@ -5,10 +5,7 @@ using System.Text;
 
 namespace UnreasonableMechanismCS
 {
-    /// <summary>
-    /// ItemEntities define all collectable items and powerups.
-    /// </summary>
-    public class ItemEntity : Entity
+    public enum ItemType
     {
     }
 }
