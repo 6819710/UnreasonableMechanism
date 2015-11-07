d81cfa295a21c64857d4a1f78874256bcf6fba9c
6819710 <j.p.galovic@gmail.com>
2015-11-08 02:34:04 +1100

    Add: defult charicter varients

--- UnreasonableMechanismCSv0.4/src/Model/Enum/PlayerType.cs
+++ UnreasonableMechanismCSv0.4/src/Model/Enum/PlayerType.cs
@@ -10,5 +10,11 @@ namespace UnreasonableMechanismCS
     /// </summary>
     public enum PlayerType
     {
+        //TODO: Rename player types with chariter names and varients.
+
+        WideA,
+        WideB,
+        NarrowA,
+        NarrowB
     }
 }
