2fdf5bc1f1bda46035eaf6d88a9bdd64fe9dc0dc
6819710 <j.p.galovic@gmail.com>
2015-11-08 04:43:40 +1100

    Add: Constructor and required method.

--- UnreasonableMechanismCSv0.4/src/Model/Movement/Linear.cs
+++ UnreasonableMechanismCSv0.4/src/Model/Movement/Linear.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UM = UnreasonableMechanismEngineCS;
+using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
 
 namespace UnreasonableMechanismCS
 {
-    public class Linear
+    public class Linear : Movement
     {
+        /// <summary>
+        /// Constructs basic linear movement.
+        /// </summary>
+        /// <param name="velocity">Velosity vector.</param>
+        public Linear(UM.Vector velocity) : base(velocity)
+        {
+        }
+
+        /// <summary>
+        /// Processes step in movement.
+        /// </summary>
+        public override void step()
+        {
+        }
     }
 }
