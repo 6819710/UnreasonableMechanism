e09543da59fa3f72d7255e03f2dccd43814016fe
6819710 <j.p.galovic@gmail.com>
2015-11-08 02:45:34 +1100

    Add: Init Bounding and Init Grazebox stubs.

--- UnreasonableMechanismCSv0.4/src/Model/PlayerEntity.cs
+++ UnreasonableMechanismCSv0.4/src/Model/PlayerEntity.cs
@@ -20,7 +20,27 @@ namespace UnreasonableMechanismCS
         public PlayerEntity(PlayerType playerType) : base(new Point(270, 430), InitBounding(new Point2D(270, 430), playerType), 1, playerType.ToString())
         {
             _playerType = playerType;
-            _grazebox = InitGrazeBox(, playerType);
+            _grazebox = InitGrazeBox(Position, playerType);
+        }
+
+        private Polygon InitBounding(Point position, PlayerType playerType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Polygon InitGrazeBox(Point position, PlayerType playerType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProcessEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProcessMovement()
+        {
+            throw new NotImplementedException();
         }
     }
 }
