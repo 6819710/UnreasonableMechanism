4dab9a8cf9acdfc3da9b4e352ea8b7198fb4e88d
6819710 <j.p.galovic@gmail.com>
2015-11-08 03:21:29 +1100

    Add: feilds, constructor and abstract methods

--- UnreasonableMechanismCSv0.4/src/Model/Movement/Movement.cs
+++ UnreasonableMechanismCSv0.4/src/Model/Movement/Movement.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UM = UnreasonableMechanismEngineCS;
+using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
 
 namespace UnreasonableMechanismCS
 {
-    public class Movement
+    /// <summary>
+    /// Movement defines basic static linear movement.
+    /// </summary>
+    public abstract class Movement
     {
+        private UM.Vector _delta;
+
+        /// <summary>
+        /// Constructs movement with provided delta.
+        /// </summary>
+        /// <param name="delta">Vector with movement information.</param>
+        public Movement(UM.Vector delta)
+        {
+            _delta = delta;
+        }
+
+        /// <summary>
+        /// Readonly Property: Delta.
+        /// </summary>
+        public UM.Vector Delta
+        {
+            get
+            {
+                return _delta;
+            }
+        }
+
+        /// <summary>
+        /// Abstract method used to change delta values.
+        /// </summary>
+        public abstract void step();
     }
 }
