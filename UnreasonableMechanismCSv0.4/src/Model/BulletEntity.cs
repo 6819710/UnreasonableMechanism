c9c4d392bceb9b44f15fe7e6d98b059e44854952
6819710 <j.p.galovic@gmail.com>
2015-11-08 02:30:41 +1100

    Add: comments, base structure.

--- UnreasonableMechanismCSv0.4/src/Model/BulletEntity.cs
+++ UnreasonableMechanismCSv0.4/src/Model/BulletEntity.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UM = UnreasonableMechanismEngineCS;
+using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
+
 
 namespace UnreasonableMechanismCS
 {
-    public class BulletEntity
+    public class BulletEntity : Entity
     {
+        private BulletColour _bulletColour;
+        private BulletType _bulletType;
+        private UM.Vector _trajectory;
+
+        public BulletEntity(Point position, UM.Vector trajectory, BulletColour bulletColour, BulletType bulletType) : base(position, InitBounding(position, bulletColour, bulletType), 1, bulletColour.ToString() + bulletType.ToString())
+        {
+            _trajectory = trajectory;
+            _bulletColour = bulletColour;
+            _bulletType = bulletType;
+        }
+
+        /// <summary>
+        /// Processes bullet events.
+        /// </summary>
+        public override void ProcessEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Processes bullet movement.
+        /// </summary>
+        public override void ProcessMovement()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Polygon InitBounding(Point position, BulletColour bulletColour, BulletType bulletType)
+        {
+            return new Polygon();
+        }
     }
 }
