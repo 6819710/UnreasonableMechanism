0a6ede0f7a4d121492d742ddf94d50295853d12a
6819710 <j.p.galovic@gmail.com>
2015-11-10 02:31:15 +1100

    Add: implementation of screen

--- UnreasonableMechanismCSv0.4/src/Screens/StarupMenu.cs
+++ UnreasonableMechanismCSv0.4/src/Screens/StarupMenu.cs
@@ -3,23 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
+
+using Colour = SwinGameSDK.Color;
+using Vector = UnreasonableMechanismEngineCS.Vector;
+
 namespace UnreasonableMechanismCS
 {
     public class StarupMenu : Screen
     {
+        private Dictionary<string, Button> _buttons = new Dictionary<string, Button>();
+        private List<string> _buttonNames = new List<string>();
+
+        /// <summary>
+        /// Constructs the startup menu
+        /// </summary>
+        public StarupMenu()
+        {
+            _buttons.Add("Play", new Button("Play", new Point(20, 20)));
+            _buttonNames.Add("Play");
+        }
+
+        /// <summary>
+        /// Draws the screen.
+        /// </summary>
         public override void Draw()
         {
-            throw new NotImplementedException();
+            SwinGame.ClearScreen(Colour.Black);
+            foreach (string btn in _buttonNames)
+            {
+                _buttons[btn].Draw();
+            }
         }
 
+        /// <summary>
+        /// Initalises the screen.
+        /// </summary>
         public override void Initalise()
         {
-            throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Processes screen events.
+        /// </summary>
         public override void ProvessEvents()
         {
-            throw new NotImplementedException();
         }
     }
 }
