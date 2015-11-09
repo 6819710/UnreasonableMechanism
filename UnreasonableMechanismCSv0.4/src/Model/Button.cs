a877434c0170fcaa43974cc050a68fb8b22f2d88
6819710 <j.p.galovic@gmail.com>
2015-11-10 02:12:44 +1100

    Add: Button Class.

---
+++ UnreasonableMechanismCSv0.4/src/Model/Button.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
+
+using Colour = SwinGameSDK.Color;
+using Vector = UnreasonableMechanismEngineCS.Vector;
+
+namespace UnreasonableMechanismCS
+{
+    /// <summary>
+    /// Defines buttons used in game screens.
+    /// </summary>
+    public class Button
+    {
+        private string _buttonText;
+        private Point _buttonLocation;
+
+        /// <summary>
+        /// Button location.
+        /// </summary>
+        /// <param name="buttonText">Text to use on button.</param>
+        /// <param name="location">Button location</param>
+        public Button(string buttonText, Point location)
+        {
+            _buttonText = buttonText;
+            _buttonLocation = location;
+        }
+
+        /// <summary>
+        /// Draws the button.
+        /// </summary>
+        public void Draw()
+        {
+            DrawText(Colour.White);
+        }
+
+        private void DrawText(Colour clr)
+        {
+            SwinGame.DrawText(_buttonText, clr, (float)_buttonLocation.X, (float)_buttonLocation.Y);
+        }
+    }
+}
