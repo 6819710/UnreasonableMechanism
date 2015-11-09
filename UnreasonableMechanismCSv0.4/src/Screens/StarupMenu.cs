7025266b1ab5937e8f097205248fa68ea2caa57d
6819710 <j.p.galovic@gmail.com>
2015-11-10 01:53:24 +1100

    Add: Startup Menu.

---
+++ UnreasonableMechanismCSv0.4/src/Screens/StarupMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismCS
+{
+    public class StarupMenu : Screen
+    {
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initalise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProvessEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
