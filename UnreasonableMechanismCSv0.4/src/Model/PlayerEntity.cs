a51489bddd6b66439ef7cd0656cccf6fb1e25a0e
6819710 <j.p.galovic@gmail.com>
2015-11-09 14:21:43 +1100

    Add: player bounding method data

--- UnreasonableMechanismCSv0.4/src/Model/PlayerEntity.cs
+++ UnreasonableMechanismCSv0.4/src/Model/PlayerEntity.cs
@@ -29,12 +29,38 @@ namespace UnreasonableMechanismCS
 
         private static Polygon InitBounding(Point position, PlayerType playerType)
         {
-            return new Polygon();
+            Point[] vertices = new Point[]
+            {
+                new Point(0,0),
+                new Point(3,0),
+                new Point(5,2),
+                new Point(6,2),
+                new Point(8,0),
+                new Point(11,0),
+                new Point(11,3),
+                new Point(10,4),
+                new Point(11,5),
+                new Point(11,10),
+                new Point(8,13),
+                new Point(9,14),
+                new Point(9,15),
+                new Point(7,17),
+                new Point(4,17),
+                new Point(2,15),
+                new Point(2,14),
+                new Point(3,13),
+                new Point(0,10),
+                new Point(0,5),
+                new Point(1,4),
+                new Point(0,3)
+            };
+
+            return new Polygon(vertices, new Point(270, 430));
         }
 
         private static Polygon InitGrazeBox(Point position, PlayerType playerType)
         {
-            return new Polygon();
+            return new Polygon(new Point[] { new Point(0, 0) }, new Point(270, 430));
         }
 
         public override void ProcessEvents()
