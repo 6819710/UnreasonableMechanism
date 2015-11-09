3f8419a4a40eb87c19c1bd629b59f3823381fcf7
6819710 <j.p.galovic@gmail.com>
2015-11-09 19:43:09 +1100

    Change: Spawn position of items.

--- UnreasonableMechanismCSv0.4/src/TestLevel.cs
+++ UnreasonableMechanismCSv0.4/src/TestLevel.cs
@@ -13,25 +13,25 @@ namespace UnreasonableMechanismCS
         private static Random _rand = new Random();
 
         private static ItemType[] _items = new ItemType[]
-            {
-                    ItemType.BigPower,
-                    ItemType.Bomb,
-                    ItemType.FullPower,
-                    ItemType.Life,
-                    ItemType.Point,
-                    ItemType.Power,
-                    ItemType.Star
-            };
+        {
+            ItemType.BigPower,
+            ItemType.Bomb,
+            ItemType.FullPower,
+            ItemType.Life,
+            ItemType.Point,
+            ItemType.Power,
+            ItemType.Star
+        };
 
         private static int[] _trigger = new int[]
         {
-                    _rand.Next()%340 + 600,
-                    _rand.Next()%340 + 40,
-                    _rand.Next()%340 + 6000,
-                    _rand.Next()%340 + 60,
-                    _rand.Next()%140 + 40,
-                    _rand.Next()%140 + 20,
-                    _rand.Next()%140 + 40
+            _rand.Next()%340 + 600,
+            _rand.Next()%340 + 40,
+            _rand.Next()%340 + 6000,
+            _rand.Next()%340 + 60,
+            _rand.Next()%140 + 40,
+            _rand.Next()%140 + 20,
+            _rand.Next()%140 + 40
         };
 
         public static void Step(uint tick)
@@ -40,7 +40,7 @@ namespace UnreasonableMechanismCS
             {
                 if (tick % (_trigger[i]) == 0 && tick > 0)
                 {
-                    GameObjects.AddItem(new ItemEntity(new Point(_rand.Next() % (460 - GameResources.GameImage("Item" + _items[i].ToString()).Width) + 40, 50), _items[i]));
+                    GameObjects.AddItem(new ItemEntity(new Point(_rand.Next() % (460 - GameResources.GameImage("Item" + _items[i].ToString()).Width) + 40, 30), _items[i]));
                 }
             }
         }
