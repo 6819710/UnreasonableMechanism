bc17f3d480bd5c29e0faac8665245aa2e307994a
6819710 <j.p.galovic@gmail.com>
2015-11-09 23:33:25 +1100

    Add: tick parameter, Screen constructor, Abstract methods.

--- UnreasonableMechanismCSv0.4/src/Screen.cs
+++ UnreasonableMechanismCSv0.4/src/Screen.cs
@@ -5,7 +5,34 @@ using System.Text;
 
 namespace UnreasonableMechanismCS
 {
-    public class Screen
+    /// <summary>
+    /// Screen defines screen types for game.
+    /// </summary>
+    public abstract class Screen
     {
+        private uint _tick;
+
+        /// <summary>
+        /// Constructs an empty screen.
+        /// </summary>
+        public Screen()
+        {
+            _tick = 0;
+        }
+
+        /// <summary>
+        /// Initalises Screen.
+        /// </summary>
+        public abstract void Initalise();
+
+        /// <summary>
+        /// Processes Screen Events.
+        /// </summary>
+        public abstract void ProvessEvents();
+
+        /// <summary>
+        /// Draws the screen.
+        /// </summary>
+        public abstract void Draw();
     }
 }
