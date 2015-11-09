9465a2b594d153912895cf31fc26951cf8dd8717
6819710 <j.p.galovic@gmail.com>
2015-11-09 23:23:24 +1100

    Add: Screen feilds, property and method to change screen.

--- UnreasonableMechanismCSv0.4/src/ScreenControler.cs
+++ UnreasonableMechanismCSv0.4/src/ScreenControler.cs
@@ -5,7 +5,24 @@ using System.Text;
 
 namespace UnreasonableMechanismCS
 {
-    public class ScreenControler
+    public static class ScreenControler
     {
+        private static Screen _screen;
+
+        /// <summary>
+        /// Readonly Property: Screen.
+        /// </summary>
+        public static Screen Screen
+        {
+            get
+            {
+                return _screen;
+            }
+        }
+
+        public static void SetScreen(Screen screen)
+        {
+            _screen = screen;
+        }
     }
 }
