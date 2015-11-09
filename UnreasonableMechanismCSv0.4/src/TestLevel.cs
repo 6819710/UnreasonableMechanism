66357f89e58f422f1b0d5da4c9e26d2ba7552871
6819710 <j.p.galovic@gmail.com>
2015-11-10 00:02:07 +1100

    Change: Updated Format.

--- UnreasonableMechanismCSv0.4/src/TestLevel.cs
+++ UnreasonableMechanismCSv0.4/src/TestLevel.cs
@@ -8,41 +8,52 @@ using SwinGameSDK;
 
 namespace UnreasonableMechanismCS
 {
-    public static class TestLevel
+    public class TestLevel : Screen
     {
-        private static Random _rand = new Random();
+        private List<ItemType> _itemType;
+        private Random _rand;
+        private List<int> _trigger;
 
-        private static ItemType[] _items = new ItemType[]
+        public override void Initalise()
         {
-            ItemType.BigPower,
-            ItemType.Bomb,
-            ItemType.FullPower,
-            ItemType.Life,
-            ItemType.Point,
-            ItemType.Power,
-            ItemType.Star
-        };
+            _itemType = new List<ItemType>(new ItemType[]
+            {
+                ItemType.BigPower,
+                ItemType.Bomb,
+                ItemType.FullPower,
+                ItemType.Life,
+                ItemType.Point,
+                ItemType.Power,
+                ItemType.Star
+            });
 
-        private static int[] _trigger = new int[]
-        {
-            _rand.Next()%340 + 600,
-            _rand.Next()%340 + 40,
-            _rand.Next()%340 + 6000,
-            _rand.Next()%340 + 60,
-            _rand.Next()%140 + 40,
-            _rand.Next()%140 + 20,
-            _rand.Next()%140 + 40
-        };
+            _rand = new Random();
+            _trigger = new List<int>(new int[]
+            {
+                _rand.Next()%340 + 600,
+                _rand.Next()%340 + 40,
+                _rand.Next()%340 + 6000,
+                _rand.Next()%340 + 60,
+                _rand.Next()%140 + 40,
+                _rand.Next()%140 + 20,
+                _rand.Next()%140 + 40
+            });
+        }
 
-        public static void Step(uint tick)
+        public override void ProvessEvents()
         {
             for (int i = 0; i < 7; ++i)
             {
-                if (tick % (_trigger[i]) == 0 && tick > 0)
+                if (Tick % (_trigger[i]) == 0 && Tick > 0)
                 {
-                    GameObjects.AddItem(new ItemEntity(new Point(_rand.Next() % (460 - GameResources.GameImage("Item" + _items[i].ToString()).Width) + 40, 30), _items[i]));
+                    GameObjects.AddItem(new ItemEntity(new Point(_rand.Next() % (460 - GameResources.GameImage("Item" + _itemType[i].ToString()).Width) + 40, 30), _itemType[i]));
                 }
             }
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
         }
     }
 }
