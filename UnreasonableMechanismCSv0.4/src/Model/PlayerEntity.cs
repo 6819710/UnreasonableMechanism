a1e5fa958c74fcfe7aa3d889c76209e4379a0afd
6819710 <j.p.galovic@gmail.com>
2015-11-09 15:28:42 +1100

    Fix: player bitmap name in constructor

--- UnreasonableMechanismCSv0.4/src/Model/PlayerEntity.cs
+++ UnreasonableMechanismCSv0.4/src/Model/PlayerEntity.cs
@@ -21,7 +21,7 @@ namespace UnreasonableMechanismCS
         /// Constructs a new player in the defult position.
         /// </summary>
         /// <param name="playerType">Type of player selected.</param>
-        public PlayerEntity(PlayerType playerType) : base(playerType.ToString(), InitBounding(new Point(270, 430), playerType), 1)
+        public PlayerEntity(PlayerType playerType) : base("Player" + playerType.ToString(), InitBounding(new Point(270, 430), playerType), 1)
         {
             _playerType = playerType;
             _grazebox = InitGrazeBox(Hitbox.Center, playerType);
