641459bae69529418f0bc8f7b4bf9309ff89c3fa
6819710 <j.p.galovic@gmail.com>
2015-11-10 02:28:38 +1100

    Change: Game main uses screens rather than direct calling.

--- UnreasonableMechanismCSv0.4/src/GameMain.cs
+++ UnreasonableMechanismCSv0.4/src/GameMain.cs
@@ -13,8 +13,6 @@ namespace UnreasonableMechanismCS
 
         private static bool _splashFlag = false;
 
-        private static uint _tick = 0;
-
         /// <summary>
         /// Main access point for the program.
         /// </summary>
@@ -25,8 +23,14 @@ namespace UnreasonableMechanismCS
 
             //Load game assets.
             GameResources.LoadResources();
-            GameObjects.Initalise();
+            GameObjects.LoadGameOjects();
+
+
             Settings.InitSettings();
+
+            ScreenControler.SetScreen("StartupMenu");
+
+            ScreenControler.Screen.Initalise();
 
             //Show SwinGame splash screen.
             if(_splashFlag)
@@ -39,31 +43,11 @@ namespace UnreasonableMechanismCS
             {
                 //Fetch the next batch of UI interaction
                 SwinGame.ProcessEvents();
+                ScreenControler.Screen.ProvessEvents();
 
-                SwinGame.ClearScreen(Color.Black);
-
-                GameObjects.ProcessEvents();
-
-                TestLevel.Step(_tick);
-
-                //Clear the screen and draw the framerate
-                
-                SwinGame.DrawFramerate(2, 2);
-
-                GameObjects.Draw();
-
-                SwinGame.DrawBitmap(GameResources.GameImage("GameArea"), 0, 0);
-                //SwinGame.DrawText("Score: " + GameScores.Score, Color.Black, 540, 40);
-                //SwinGame.DrawText("Lives: " + GameScores.Player, Color.Black, 540, 60);
-                //SwinGame.DrawText("Bombs: " + GameScores.Bomb, Color.Black, 540, 80);
-                //SwinGame.DrawText("Power: " + GameScores.Power, Color.Black, 540, 120);
-                //SwinGame.DrawText("Graze: " + GameScores.Graze, Color.Black, 540, 140);
-                //SwinGame.DrawText("Bonus: " + GameScores.Bonus, Color.Black, 540, 160);
-
-                //Draw onto the screen
+                //Draw Game
+                ScreenControler.Screen.Draw();
                 SwinGame.RefreshScreen(60);
-
-                _tick++;
             }
 
             //Free game assets.
