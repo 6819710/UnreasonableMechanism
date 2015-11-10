63a000a141adb1dc3ed1b4d42f19d273d2b08f69
6819710 <j.p.galovic@gmail.com>
2015-11-10 18:16:07 +1100

    Add: inial implementation.

--- UnreasonableMechanismCSv0.4/src/Screens/TestLevel.cs
+++ UnreasonableMechanismCSv0.4/src/Screens/TestLevel.cs
@@ -3,9 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
+
+using Colour = SwinGameSDK.Color;
+using Vector = UnreasonableMechanismEngineCS.Vector;
+
 namespace UnreasonableMechanismCS
 {
     public class TestLevel : Screen
     {
+        private Random _rand;
+
+        public TestLevel()
+        {
+            _rand = new Random();
+        }
+
+        public override void Draw()
+        {
+            SwinGame.ClearScreen(Color.DarkSlateGray);
+
+            GameObjects.DrawPlayer();
+
+            SwinGame.DrawBitmap(GameResources.GameImage("GameArea"), 0, 0);
+        }
+
+        public override void Initalise()
+        {
+            
+        }
+
+        public override void ProvessEvents()
+        {
+            GameObjects.Player.ProcessEvents();
+        }
     }
 }
