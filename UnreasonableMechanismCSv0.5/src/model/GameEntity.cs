88d0317647631d5ff998604a02ef928a464187f9
6819710 <j.p.galovic@gmail.com>
2015-11-15 05:48:54 +1100

    Add: Inital GameEntity Add

---
+++ UnreasonableMechanismCSv0.5/src/model/GameEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismCS
+{
+    public class GameEntity : GameObject
+    {
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
