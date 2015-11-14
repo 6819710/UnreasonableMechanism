6d81c1f4035c5542dac6123d0684b0cb23964d29
6819710 <j.p.galovic@gmail.com>
2015-11-15 09:59:26 +1100

    Add: Draw Bitmap Method, Bitmap property and extra constructors.

--- UnreasonableMechanismCSv0.5/src/GameObject.cs
+++ UnreasonableMechanismCSv0.5/src/GameObject.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 
 using UnreasonableMechanismEngineCS;
+using SwinGameSDK;
+
+using Vector = UnreasonableMechanismEngineCS.Vector;
 
 namespace UnreasonableMechanismCS
 {
@@ -12,33 +15,66 @@ namespace UnreasonableMechanismCS
     /// </summary>
     public abstract class GameObject
     {
+        private string _bitmap;
         private Point _position;
 
         /// <summary>
-        /// Constructs Game Object in defult position.
+        /// Constructs Game Object in defualt position.
         /// </summary>
-        public GameObject()
-        {
-            _position = new Point(0, 0);
-        }
+        public GameObject() : this(new Point(0,0), ""){}
+
+        /// <summary>
+        /// Construcst Game Object in default position using given bitmap name.
+        /// </summary>
+        /// <param name="bitmap">Bitmap name.</param>
+        public GameObject(string bitmap) : this(new Point(0,0), bitmap){}
 
         /// <summary>
         /// Constructs Game Object using given position.
         /// </summary>
         /// <param name="position">Position of Object.</param>
-        public GameObject(Point position)
-        {
-            _position = position;
-        }
+        public GameObject(Point position) : this(position, ""){}
 
         /// <summary>
         /// Constructs Game Object using given coordinates.
         /// </summary>
         /// <param name="x">X coordinate.</param>
         /// <param name="y">Y coordinate.</param>
-        public GameObject(double x, double y)
+        public GameObject(double x, double y) : this(new Point(x, y), ""){}
+
+        /// <summary>
+        /// Constructs Game Object using given coordinates and bitmap name.
+        /// </summary>
+        /// <param name="x">X coordinate.</param>
+        /// <param name="y">Y coordinate.</param>
+        /// <param name="bitmap">Bitmap name.</param>
+        public GameObject(double x, double y, string bitmap) : this(new Point(x, y), bitmap) { }
+
+        /// <summary>
+        /// Constructs Game Object using given point and bitmap name.
+        /// </summary>
+        /// <param name="position">Position of Object.</param>
+        /// <param name="bitmap">Bitmap name.</param>
+        public GameObject(Point position, string bitmap)
         {
-            _position = new Point(x, y);
+            _position = position;
+            _bitmap = bitmap;
+        }
+
+        /// <summary>
+        /// Property: Bitmap name.
+        /// </summary>
+        public string Bitmap
+        {
+            get
+            {
+                return _bitmap;
+            }
+
+            set
+            {
+                _bitmap = value;
+            }
         }
 
         /// <summary>
@@ -56,6 +92,14 @@ namespace UnreasonableMechanismCS
         /// Draws Game Object.
         /// </summary>
         public abstract void Draw();
+
+        /// <summary>
+        /// Draws Game Object Bitmap.
+        /// </summary>
+        public virtual void DrawBitmap()
+        {
+            //SwinGame.DrawBitmap(BITMAP, )
+        }
 
         /// <summary>
         /// Offsets Game Object by given Movement Vector.
