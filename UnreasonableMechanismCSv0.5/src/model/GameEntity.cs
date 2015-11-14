2fe0fd35eb066aa44d34fa7ec33a9e16efac8c82
6819710 <j.p.galovic@gmail.com>
2015-11-15 10:04:49 +1100

    Add: basic information

--- UnreasonableMechanismCSv0.5/src/model/GameEntity.cs
+++ UnreasonableMechanismCSv0.5/src/model/GameEntity.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using UnreasonableMechanismEngineCS;
+
 namespace UnreasonableMechanismCS
 {
+    /// <summary>
+    /// GameEntity is a Child Class of GameObject defining Game Objects with Collision Dectection.
+    /// </summary>
     public class GameEntity : GameObject
     {
+        private Polygon _hitbox;
+        private int _hitpoints;
+        private bool _remove;
+
+        public GameEntity(Point position, Polygon hitbox, int hitpoints) : base(position)
+        {
+            _hitbox = hitbox;
+            _hitpoints = hitpoints;
+            _remove = false;
+        }
+
         public override void Draw()
         {
             throw new NotImplementedException();
