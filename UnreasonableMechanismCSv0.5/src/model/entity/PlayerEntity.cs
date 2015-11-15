263dd755eddff730059a86ebbf782ef2a8f69316
6819710 <j.p.galovic@gmail.com>
2015-11-15 22:23:09 +1100

    Add: inital add

---
+++ UnreasonableMechanismCSv0.5/src/model/entity/PlayerEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnreasonableMechanismEngineCS;
+
+namespace UnreasonableMechanismCS
+{
+    public class PlayerEntity : GameEntity
+    {
+        private PlayerType _playerType;
+        private Polygon _grazebox;
+
+        private int _cannonMain;
+        private int _cannonAux;
+        private int _cooldown;
+
+        public PlayerEntity()
+    }
+}
