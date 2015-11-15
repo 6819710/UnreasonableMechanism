b9cd70d8197ec300f3a2373fda51544730b32864
6819710 <j.p.galovic@gmail.com>
2015-11-16 02:40:14 +1100

    Add: Engine v0.3

--- UnreasonableMechanismCSv0.5/src/model/GameEntity.cs
+++ UnreasonableMechanismCSv0.5/src/model/GameEntity.cs
@@ -16,6 +16,26 @@ namespace UnreasonableMechanismCS
         private int _hitpoints;
         private bool _remove;
 
+        /// <summary>
+        /// Constructs empty GameEntity.
+        /// </summary>
+        public GameEntity()
+        { }
+
+        /// <summary>
+        /// Constructs GameEntity using given hitbox.
+        /// </summary>
+        /// <param name="hitbox"></param>
+        public GameEntity(Polygon hitbox) : this(hitbox.Middle, hitbox, 1)
+        { }
+
+        /// <summary>
+        /// Constructs GameEntity using given position, hitbox and hitpoints.
+        /// </summary>
+        /// <param name="position">Position of entity</param>
+        /// <param name="hitbox">Hitbox.</param>
+        /// <param name="hitpoints">Hitpoints.</param>
+
         public GameEntity(Point position, Polygon hitbox, int hitpoints) : base(position)
         {
             _hitbox = hitbox;
