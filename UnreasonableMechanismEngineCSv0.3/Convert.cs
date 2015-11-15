5c3fd8d148e29a893265f9add28adb9264915c9b
6819710 <j.p.galovic@gmail.com>
2015-11-16 02:50:35 +1100

    Add: Convert Static Class Remove: Irrelivant files and folders.

---
+++ UnreasonableMechanismEngineCSv0.3/Convert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismEngineCS
+{
+    /// <summary>
+    /// Convert is a Static Class containing definitions for convertion methods.
+    /// </summary>
+    public static class Convert
+    {
+        /// <summary>
+        /// Converts Degrees to Radians.
+        /// </summary>
+        /// <param name="angle">Angle in Degrees.</param>
+        /// <returns>Angle in Radians.</returns>
+        public static double DegToRad(double angle)
+        {
+            return angle * (Math.PI / 180.0);
+        }
+
+        /// <summary>
+        /// Converts Radians to Degrees.
+        /// </summary>
+        /// <param name="angle">Angle in Radians.</param>
+        /// <returns>Angle in Degrees.</returns>
+        public static double RadToDeg(double angle)
+        {
+            return angle * (Math.PI / 180.0);
+        }
+    }
+}
