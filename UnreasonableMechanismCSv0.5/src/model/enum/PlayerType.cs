feadf9a86dd18142a5b50c03b4e5ff0f45411aaf
6819710 <j.p.galovic@gmail.com>
2015-11-15 22:22:55 +1100

    Add: Inital add

---
+++ UnreasonableMechanismCSv0.5/src/model/enum/PlayerType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnreasonableMechanismCS
+{
+    public enum PlayerType
+    {
+        //TODO: Rename player types with chariter names and varients.
+
+        WideA,
+        WideB,
+        NarrowA,
+        NarrowB
+    }
+}
